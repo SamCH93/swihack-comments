6b956d6838dc826611be5325d12896eb60847239
murezzda <47388020+murezzda@users.noreply.github.com>
2020-02-21 14:21:37 +0100

    Comments are parsed

--- parser.py
+++ parser.py
@@ -16,7 +16,27 @@ def parse_article(link):
 
     return text, translator, comments
 
-def parse_comments(id):
+
+def parse_comments(id, lang):
+    language_map = {
+        "German": ("ger", 40000532),
+        "English": ("eng", 40000108),
+        "Arabic": ("ara", 40000690),
+        "Spanish": ("spa", 40000430),
+        "French": ("fre", 40000920),
+        "Italian": ("ita", 40001010),
+        "Japanese": ("jpn", 40000268),
+        "Portuguese": ("por", 40000082),
+        "Chinese": ("chi", 40000610),
+        "Russian": ("rus", 40000348)
+        }
+
+    lstring, lcode = language_map[lang]
+
+    comment_response = requests.get(f"https://www.swissinfo.ch/elastic/social/{lstring}/comments?teasable=contentbean:{id}&cmnavigation=contentbean:{lcode}&numberOfComments=100")
+    soup = BeautifulSoup(comment_response.content, features='lxml')
+    return [x.text for x in soup.find_all("div", attrs={"class": "user-comment"})]
+
 
 result = requests.get("https://www.swissinfo.ch/service/eng/rssxml/top-news/rss")
 
@@ -24,7 +44,7 @@ rss_xml = result.content.decode("UTF-8")
 rss_dict = xmltodict.parse(rss_xml)
 articles = rss_dict['rss']['channel']['item']
 
-article_id_matcher = re.compile("https://www.swissinfo.ch/.*?/.*?/(?P<articleid>\d+)\?.*")
+article_id_matcher = re.compile("https://www.swissinfo.ch/.*?/.*?/(?P<articleid>\d+).*")
 
 for article in articles:
     soup = BeautifulSoup(requests.get(article["link"]).content, features='lxml')
@@ -35,21 +55,19 @@ for article in articles:
     languages = [x.split()[0] for x in languages]
     links = [x.findChildren('a')[0]['href'] for x in other_lang_nodes]
 
-    languages.append("Englisch")
+    languages.append("English")
     links.append(article['link'])
 
     article['content'] = {}
 
     for link, language in zip(links, languages):
         article['content'][language] = {}
-        article['content'][language]['id'] = article_id_matcher.match(article['link']).group("articleid")
+        article['content'][language]['id'] = article_id_matcher.match(link).group("articleid")
 
         text, translator, comments = parse_article(link)
         article['content'][language]['text'] = text
         article['content'][language]['translator'] = translator
-        article['content'][language]['comments'] = comments
-
-        comments = parse_comments(article['content'][language]['id'])
+        article['content'][language]['comments'] = parse_comments(article['content'][language]['id'], language)
 
     with open("articles.json", "w", encoding="UTF-8") as articles_json:
         json.dump(articles, articles_json, ensure_ascii=False, indent=2)
